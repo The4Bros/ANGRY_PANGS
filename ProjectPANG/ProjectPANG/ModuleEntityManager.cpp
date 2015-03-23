4ac8be4714361107d99a9eea1720b2de2c49729e
cumus <ec4who@hotmail.com>
2015-03-24 00:02:10 +0100

    app->inputModule->key not commented to throw error

--- ProjectPANG/ProjectPANG/ModuleEntityManager.cpp
+++ ProjectPANG/ProjectPANG/ModuleEntityManager.cpp
@@ -7,7 +7,7 @@ ModuleEntityManager::ModuleEntityManager(Application* app) : Module(app)
 }
 bool ModuleEntityManager::Init()
 {
-	
+
 	background_const_source_rect = { 8, 8, 384, 208 };
 	background_const_rect = new SDL_Rect(background_const_source_rect);
 
@@ -20,7 +20,7 @@ update_status ModuleEntityManager::PreUpdate(){ return UPDATE_CONTINUE; }
 update_status ModuleEntityManager::Update()
 {
 	
-	//if (app->inputModule->key[SDLK_LEFT]){ character_rect.x -= 1; }
+	if (app->inputModule->key[SDLK_LEFT]){ character_rect.x -= 1; }
 	//if (app->inputModule->key[SDLK_RIGHT]){ character_rect.x += 1; }
 	//if (app->inputModule->key[SDLK_UP]){ character_rect.y -= 1; }
 	//if (app->inputModule->key[SDLK_DOWN]){ character_rect.y += 1; }
