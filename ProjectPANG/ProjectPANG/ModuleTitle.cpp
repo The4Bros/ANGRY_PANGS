3660a556824d89afec0f481aad3daa4bb1e1ca19
auusi9 <ausi_dalmau@hotmail.com>
2015-04-16 21:13:18 +0200

    Module Title comenzado

--- ProjectPANG/ProjectPANG/ModuleTitle.cpp
+++ ProjectPANG/ProjectPANG/ModuleTitle.cpp
@@ -4,8 +4,7 @@ ModuleTitle::ModuleTitle(Application* app) : Module(app){}
 
 bool ModuleTitle::Init()
 {
-	background_rect = { 77, 29, 230 * app->windowModule->scale, 150 * app->windowModule->scale };
-	insert_coin_rect = { 77, 29, 150 * app->windowModule->scale, 140 * app->windowModule->scale };
+	
 	//app->current_time = 0;
 	return true;
 }
@@ -16,8 +15,41 @@ update_status ModuleTitle::PreUpdate()
 	return UPDATE_CONTINUE;
 }
 update_status ModuleTitle::Update()
-{
-	if (app->current_time > 600 || app->inputModule->key[SDL_SCANCODE_SPACE] == 1){ return CHANGE_TO_PLAY; }
+{	
+	int resto = app->current_time % 2;
+	//Print inital screens
+	if (app->current_time < 10){
+		SDL_Rect tmp = { 0, 0, 384 * app->windowModule->scale, 240 * app->windowModule->scale };
+		SDL_Rect tmp2 = { 0, 0, 384 , 240};
+		app->renderModule->Print(app->texturesModule->title_sprite, &tmp2, &tmp);
+	}
+	else{
+		if (app->current_time < 20){
+			SDL_Rect tmp = { 0, 0, 384 * app->windowModule->scale, 240 * app->windowModule->scale };
+			SDL_Rect tmp2 = { 384, 0, 384, 240 };
+			app->renderModule->Print(app->texturesModule->title_sprite, &tmp2, &tmp);
+		}
+		else{
+			if (app->current_time < 150){
+				SDL_Rect tmp = { 0, 0, 384 * app->windowModule->scale, 240 * app->windowModule->scale };
+				SDL_Rect tmp2 = { 768, 0, 384, 240 };
+				app->renderModule->Print(app->texturesModule->title_sprite, &tmp2, &tmp);
+			}
+			else{
+				SDL_Rect tmp = { 0, 0, 384 * app->windowModule->scale, 240 * app->windowModule->scale };
+				SDL_Rect tmp2 = { 1152, 0, 384, 240 };
+				app->renderModule->Print(app->texturesModule->title_sprite, &tmp2, &tmp);
+				SDL_Rect tmp4 = { 100 * app->windowModule->scale, 200 * app->windowModule->scale, 177 * app->windowModule->scale, 15 * app->windowModule->scale };
+				SDL_Rect tmp3 = { 1536, 0, 176, 15 };
+				if (resto == 0)app->renderModule->Print(app->texturesModule->title_sprite, &tmp3, &tmp4); 
+			}
+		}
+	}
+
+
+
+
+	if (app->current_time > 500 || app->inputModule->key[SDL_SCANCODE_SPACE] == 1){ return CHANGE_TO_PLAY; }
 	return UPDATE_CONTINUE;
 }
 	
