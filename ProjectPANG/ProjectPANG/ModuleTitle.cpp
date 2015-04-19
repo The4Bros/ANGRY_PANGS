271c975b1387c9fc90c26201b980318183e11caa
auusi9 <ausi_dalmau@hotmail.com>
2015-04-19 15:34:00 +0200

    Title sprites

--- ProjectPANG/ProjectPANG/ModuleTitle.cpp
+++ ProjectPANG/ProjectPANG/ModuleTitle.cpp
@@ -5,7 +5,9 @@ bool ModuleTitle::Init()
 {	
 	app->current_time = 0;
 	ticks = 0;
-	dir = 1;
+	for (int i = 0; i < 4; i++){ dir[i] = 1; aux[i] = 1; }
+	
+	
 	rect = { 0, 0, 384 * app->windowModule->scale, 240 * app->windowModule->scale };
 	insert_coin_rect = { 100 * app->windowModule->scale, 200 * app->windowModule->scale, 177 * app->windowModule->scale, 15 * app->windowModule->scale };
 
@@ -17,6 +19,12 @@ bool ModuleTitle::Init()
 	balloon_rects[2] = { 384*app->windowModule->scale, 0, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
 	balloon_rects[3] = { 0, 0, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
 	balloon_source_rect = new SDL_Rect({ 0, 0, 48, 40 });
+
+	balloon_split[0] = { 72 * app->windowModule->scale, 37 * app->windowModule->scale, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
+	balloon_split[1] = { 131 * app->windowModule->scale, 52 * app->windowModule->scale, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
+	balloon_split[2] = { 193 * app->windowModule->scale, 28 * app->windowModule->scale, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
+	balloon_split[3] = { 232 * app->windowModule->scale, 49 * app->windowModule->scale, 48 * app->windowModule->scale, 40 * app->windowModule->scale };
+	for (int i = 0; i < 4; i++){ balloon_split_source_rect[i] = new SDL_Rect({ (i * 48)+1536, 167, 48, 40 }); }
 
 	insert_coin_pressed = false;
 
@@ -39,12 +47,12 @@ update_status ModuleTitle::Update()
 		//begginning animation:
 		if (app->current_time < 5)
 		{
+
 			Update_Balloons();
-			
 		}
 		// insert coin 
 		else
-		{
+		{	
 			app->renderModule->Print(app->texturesModule->title_sprite, source_rect[3], &rect);
 			if (ticks % 60 < 30) { app->renderModule->Print(app->texturesModule->title_sprite, source_rect[4], &insert_coin_rect); }
 
@@ -83,16 +91,33 @@ bool ModuleTitle::CleanUp(){ return true; }
 
 void ModuleTitle::Update_Balloons()//Balls title animation 
 {
-	balloon_rects[0].x -= 2*app->windowModule->scale;
-	//balloon_rects[1].x += app->windowModule->scale;
-	//balloon_rects[2].x -= app->windowModule->scale;
-	//balloon_rects[3].x += app->windowModule->scale;
+	app->fontManagerModule->Write_On_Screen(ticks, 50*2, 100, 8*app->windowModule->scale);
+	//P
+	balloon_rects[0].x -= 8*app->windowModule->scale;
+	balloon_rects[0].y += (aux[0] * 3 * app->windowModule->scale) + (dir[0] * 4.5*(ticks / 150)*(ticks / 150));
 
-	balloon_rects[0].y += (app->windowModule->scale) + (dir*0.09*(ticks /10)*(ticks / 10));
-	//balloon_rects[1].y += app->windowModule->scale;
-	//balloon_rects[2].y += app->windowModule->scale;
-	//balloon_rects[3].y += app->windowModule->scale;
-	if (balloon_rects[0].y > 208 * app->windowModule->scale){ dir = -1; }
+	//A
+	if (ticks > 360){ balloon_rects[1].x += 8 * app->windowModule->scale;
+	balloon_rects[1].y += (aux[1] * 3 * app->windowModule->scale) + (dir[1] * 4.5*(ticks / 150)*(ticks / 150));
+	}
+	
+	
+
+	//N
+	if (ticks > 365){
+		balloon_rects[2].y += (aux[2] * 3 * app->windowModule->scale) + (dir[2] * 4.5*(ticks / 150)*(ticks / 150));
+		
+		balloon_rects[2].x -= 8 * app->windowModule->scale;
+	}
+	//G
+	if (ticks > 370){
+	balloon_rects[3].x += 8 * app->windowModule->scale;
+	balloon_rects[3].y += (aux[3] * 3 * app->windowModule->scale) + (dir[3] * 4.5*(ticks / 150)*(ticks / 150));
+	}
+
+	for (int i = 0; i < 4; i++){ if (balloon_rects[i].y > 208 * app->windowModule->scale){ dir[i] = -1; aux[i] = -0.8; } }
 
 	for (int i = 0; i < 4; i++){ app->renderModule->Print(app->texturesModule->balls_sprite, balloon_source_rect, &balloon_rects[i]);  }
+
+
 }
--- ProjectPANG/ProjectPANG/ModuleTitle.h
+++ ProjectPANG/ProjectPANG/ModuleTitle.h
@@ -14,10 +14,12 @@ public:
 	SDL_Rect rect;
 	SDL_Rect insert_coin_rect;
 	SDL_Rect* source_rect[5];
-	int dir;
+	int dir[4];
+	int aux[4];
 	SDL_Rect balloon_rects[4];
 	SDL_Rect* balloon_source_rect;
-
+	SDL_Rect balloon_split[4];
+	SDL_Rect* balloon_split_source_rect[4];
 	bool insert_coin_pressed;
 
 	ModuleTitle(Application* app);
